1add85f86ff9fdfeffbb8a39e32cfa61e2b33d85
Dmytro Mytrokhin <mitrohin21020@gmail.com>
2023-11-08 20:27:38 +0200

    add: weather details per day

--- src/components/CurrentWeather/CurrentWeather.tsx
+++ src/components/CurrentWeather/CurrentWeather.tsx
@@ -2,8 +2,26 @@ import { formatTemperature } from '../../utils/weatherUtils.ts';
 import { ICurrentWeather } from '../../types/ICurrentWeather.ts';
 import './CurrentWeather.scss';
 import { weatherIcons } from '../../utils/weatherIcons.ts';
+import { DateTime, FixedOffsetZone } from 'luxon';
 
 export const CurrentWeather = ({ weatherData, unitsType }: ICurrentWeather) => {
+  const getFormattedDate = (
+    secs: number | undefined,
+    zone: number | undefined,
+  ) => {
+    if (secs !== undefined && zone !== undefined) {
+      const offsetInMinutes = zone / 60;
+      const customZone = FixedOffsetZone.instance(offsetInMinutes);
+      const dateTime = DateTime.fromSeconds(secs).setZone(customZone);
+
+      const date = dateTime.toFormat('cccc, dd.LL.yyyy');
+      const time = dateTime.toFormat('hh:mm a');
+
+      return { date, time };
+    }
+    return { date: 'Unknown Date', time: 'Unknown Time' };
+  };
+
   return (
     <div className="current-weather">
       {weatherData && (
@@ -17,15 +35,19 @@ export const CurrentWeather = ({ weatherData, unitsType }: ICurrentWeather) => {
             {formatTemperature(weatherData.main.temp, unitsType)}
           </span>
           <span className="current-weather__name">
-            {weatherData.weather[0].main}
+            {weatherData.weather[0].description}
           </span>
 
           <div className="current-weather__location-info">
-            <span>3.10.2023</span>
+            <span>
+              {getFormattedDate(weatherData?.dt, weatherData?.timezone).date}
+            </span>
             <span>
               {weatherData.name}, {weatherData.sys.country}
             </span>
-            <span>Monday, 10:10</span>
+            <span>
+              {getFormattedDate(weatherData?.dt, weatherData?.timezone).time}
+            </span>
           </div>
         </>
       )}
--- src/utils/weatherDetails.ts
+++ src/utils/weatherDetails.ts
@@ -7,12 +7,21 @@ import pressure_icon from '../assets/images/pressure.png';
 import wind_icon from '../assets/images/windy.png';
 import sunrise_icon from '../assets/images/sunrise.png';
 import sunset_icon from '../assets/images/sunset.png';
-import { WeatherData } from '../types/IWeather.ts';
+import { IWeatherInfo } from '../components/WeatherDetails/WeatherDetails.tsx';
 
-export const weatherDetails = (
-  weatherData: WeatherData | null,
-  unitsType: string,
-) => {
+export const weatherDetails = ({
+  feels_like,
+  temp_min,
+  temp_max,
+  sunrise,
+  sunset,
+  humidity,
+  pressure,
+  wind,
+  visibility,
+  timezone,
+  unitsType,
+}: IWeatherInfo) => {
   const formatLocalTime = (timestamp: number, timezoneOffset: number) => {
     const utcTime = new Date(timestamp * 1000);
     const localTime = new Date(utcTime.getTime() + timezoneOffset * 1000);
@@ -22,52 +31,48 @@ export const weatherDetails = (
     });
   };
 
-  const sunrise = weatherData
-    ? formatLocalTime(weatherData.sys.sunrise, weatherData.timezone)
-    : '';
-  const sunset = weatherData
-    ? formatLocalTime(weatherData.sys.sunset, weatherData.timezone)
-    : '';
+  const formattedSunrise = formatLocalTime(sunrise, timezone);
+  const formattedSunset = formatLocalTime(sunset, timezone);
 
-  return weatherData
-    ? [
-        {
-          label: 'Feels like',
-          icon: feels_icon,
-          value: formatTemperature(weatherData.main.feels_like, unitsType),
-        },
-        {
-          label: 'Min Temp',
-          icon: minTemp_icon,
-          value: formatTemperature(weatherData.main.temp_min, unitsType),
-        },
-        {
-          label: 'Max Temp',
-          icon: maxTemp_icon,
-          value: formatTemperature(weatherData.main.temp_max, unitsType),
-        },
-        {
-          label: 'Humidity',
-          icon: humidity_icon,
-          value: `${weatherData.main.humidity}%`,
-        },
-        {
-          label: 'Pressure',
-          icon: pressure_icon,
-          value: `${weatherData.main.pressure}mb`,
-        },
-        {
-          label: 'Wind',
-          icon: wind_icon,
-          value: `${weatherData.wind.speed} km/h`,
-        },
-        { label: 'Sunrise', icon: sunrise_icon, value: `${sunrise}` },
-        { label: 'Sunset', icon: sunset_icon, value: `${sunset}` },
-        {
-          label: 'Visibility',
-          icon: feels_icon,
-          value: `${weatherData.visibility} m`,
-        },
-      ]
-    : [];
+  const details = [
+    {
+      label: 'Feels like',
+      icon: feels_icon,
+      value: formatTemperature(feels_like, unitsType),
+    },
+    {
+      label: 'Min Temp',
+      icon: minTemp_icon,
+      value: formatTemperature(temp_min, unitsType),
+    },
+    {
+      label: 'Max Temp',
+      icon: maxTemp_icon,
+      value: formatTemperature(temp_max, unitsType),
+    },
+    {
+      label: 'Humidity',
+      icon: humidity_icon,
+      value: `${humidity}%`,
+    },
+    {
+      label: 'Pressure',
+      icon: pressure_icon,
+      value: `${pressure}mb`,
+    },
+    {
+      label: 'Wind',
+      icon: wind_icon,
+      value: `${wind} km/h`,
+    },
+    { label: 'Sunrise', icon: sunrise_icon, value: formattedSunrise },
+    { label: 'Sunset', icon: sunset_icon, value: formattedSunset },
+    {
+      label: 'Visibility',
+      icon: feels_icon,
+      value: `${visibility} m`,
+    },
+  ];
+
+  return details;
 };
